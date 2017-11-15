788110f6487a0edba8e68573113b3562f9a62e8b
TheKotiks <mirina1507@gmail.com>
2017-11-15 21:18:34 +0300

    12
    12

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
+
+	//alert('w = ' + x + ', h = ' + y);
+	//alert($('.brands').width());
+});
